73bbe7a82d64a146fd661345b242e0a8effee51a
fati <fatemesadatsedigh@gmail.com>
2025-04-14 18:54:12 +0330

    add public Thermostat

--- src/device/Thermostat.java
+++ src/device/Thermostat.java
@@ -5,6 +5,12 @@ import protocol.Protocol;
 public class Thermostat extends Device {
     public int temperature;
 
+    public Thermostat (String name, Protocol protocol){
+        super(name, protocol);
+        this.temperature = 20;
+    }
+
+
 
 
 }
