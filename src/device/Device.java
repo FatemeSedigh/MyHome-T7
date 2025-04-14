8928058a4cd018a058f105b3e5be726d2c70e65d
fati <fatemesadatsedigh@gmail.com>
2025-04-14 12:11:35 +0330

    add fiels in device/Device

--- src/device/Device.java
+++ src/device/Device.java
@@ -1,4 +1,13 @@
 package device;
 
+import protocol.Protocol;
+
 public class Device {
+
+    protected String name;
+    protected String status;
+    protected Protocol protocol;
+
+
+
 }
--- src/protocol/Wifi.java
+++ src/protocol/Wifi.java
@@ -1,4 +1,4 @@
-package protocols;
+package protocol;
 
 public class Wifi implements Protocol {
 
